9981b180c9c73abc0257345b410c0a81b97d504a
yusukeishino <yskgori0407@gmail.com>
2021-01-12 14:18:08 +0000

    スクレイピング

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -4,9 +4,17 @@ class HomesController < ApplicationController
   end
 
   def news
-   @agent = Mechanize.new
-   @page = @agent.get("https://hobby.dengeki.com/")
-   @elements = @page.search('a')
+   #@agent = Mechanize.new
+   #@page = @agent.get("https://hobby.dengeki.com/tag/gunpla-2/")
+   #@elements = @page.search('div.topnews')
+   agent = Mechanize.new
+   page = agent.get("https://hobby.dengeki.com/tag/gunpla-2/")
+   @elements = page.at('div.topnews')
+   # elements.css('.ttlB').children.first.attributes['href'].value
+   # @elements.class
+   # @elements.methods
+   # @elements.css('.ttlB').methods
+   @elements = @elements.css('.ttlB').children
   end
 
 end
