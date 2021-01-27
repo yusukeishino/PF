19057702b730b9d8618b7e87db873ecc2544a74a
yusukeishino <yskgori0407@gmail.com>
2021-01-27 12:09:38 +0000

    search fix

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -7,6 +7,5 @@ FactoryBot.define do
     trait :body_over do
       body { Faker::Lorem.characters(number:51)}
     end
-    #image {Rack::Test::UploadedFile.new('app/assets/images/no_image.jpg', 'image/jpeg')}
   end
 end
