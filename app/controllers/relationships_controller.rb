0a777e08cc085f308032be3fe9df1d295fbb7ece
yusukeishino <yskgori0407@gmail.com>
2021-02-14 06:16:09 +0000

    follow ajax

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,13 +1,15 @@
 class RelationshipsController < ApplicationController
 
  def follow
+  @user = User.find(params[:id])
   current_user.follow(params[:id])
-  redirect_to request.referer
+  #redirect_to request.referer
  end
 
 def unfollow
+  @user = User.find(params[:id])
   current_user.unfollow(params[:id])
-  redirect_to request.referer
+  #redirect_to request.referer
 end
 
 end
